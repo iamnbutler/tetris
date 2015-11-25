74ed58a503d664d72b7920d38adb1a82351a2bcf
Nate Butler <iamnbutler@gmail.com>
2015-11-25 14:26:17 -0500

    disable blocks until we work out grid

--- blocks.ino
+++ blocks.ino
@@ -1,20 +1,46 @@
 // TODO: Make Blocks
-	
-	// TODO: protection against the same block coming up more than twice in a row
-
-int block[]					= {0,0,0,0,0,0}, // Block data: {xPos, yPos, blockType, rotation}
-		xPos						= 0,
-		yPos						= 0,
-		blockWidth			= 0,
-		blockHeight			= 0,
-		blockTypes			= 0, // Block blockTypes: square = 0, T = 1, L = 2, J = 3, I = 4, S = 5, Z = 6
-		rotation				= 0, // Rotation in degrees, can be 0, 90, 180, 270
-		blockSize				= 20; // Relates to grid size
-
-// Block Colors
 
 
-// TODO: Create next block
-int nextBlock() {
+// int blockLayout[][]	= {
+// 												{0,0,0},
+// 												{0,0,0},
+// 												{0,0,0},
+// 												{0,0,0}
+// 											}
+// 		block[]					= {0,0,0,0,0}
+// 		xPos						= 0,
+// 		yPos						= 0,
+// 		blockWidth			= 0,
+// 		blockHeight			= 0,
+// 		blockType 			= 0, // Block blockTypes: square = 0, T = 1, L = 2, J = 3, I = 4, S = 5, Z = 6
+// 		rotation				= 0, // Rotation in degrees, can be 0, 90, 180, 270
 
-}
+// // Block Colors
+
+
+// // TODO: Create next block
+// int newBlock() {
+// 	// TODO: protection against the same block coming up more than twice in a row
+// 	blockType = random(0,1); // Make a new random block
+
+// 	switch (blockType) { // Define block layouts
+//     case 0:
+//       blockLayout = {
+//       	{0,0,0},
+// 				{0,0,0},
+// 				{1,1,0},
+// 				{1,1,0}
+//       }
+//       break;
+//     default: 
+//     	blockLayout = {
+// 	      {0,0,0},
+// 				{0,0,0},
+// 				{0,1,0},
+// 				{1,1,1}
+// 			}
+//     break;
+//   }
+
+// 	int currentBlock = block[0,0,2,2,blockType,0];
+// }
