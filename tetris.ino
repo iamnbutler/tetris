84f3d1b87d0cb5a16d198cb0e6e6f8e2cd4fa624
Nate Butler <iamnbutler@gmail.com>
2015-11-25 14:25:51 -0500

    todo counter, add grid function

--- tetris.ino
+++ tetris.ino
@@ -10,37 +10,29 @@
 #define C   A2
 RGBmatrixPanel matrix(A, B, C, CLK, LAT, OE, false);
 
+// Build counter to define refresh rate (15fps?)
+
 // TODO: Start Game
 
-void setup() { 
-	matrix.begin();
+void setup() {
+  Serial.begin(9600);
+  matrix.begin();
 }
 
 void loop() {
-	
-
-
-	// T
-	matrix.drawPixel(31,0, matrix.Color333(200, 110, 70));
-	matrix.drawPixel(31,1, matrix.Color333(200, 110, 70));
-	matrix.drawPixel(31,2, matrix.Color333(200, 110, 70));
-	matrix.drawPixel(30,1, matrix.Color333(200, 110, 70));
-
-	// L
-	matrix.drawPixel(31,3, matrix.Color333(120, 110, 200));
-	matrix.drawPixel(31,4, matrix.Color333(120, 110, 200));
-	matrix.drawPixel(30,4, matrix.Color333(120, 110, 200));
-	matrix.drawPixel(29,4, matrix.Color333(120, 110, 200));
-
-	// S
-	matrix.drawPixel(31,5, matrix.Color333(170, 220, 200));
-	matrix.drawPixel(31,6, matrix.Color333(170, 220, 200));
-	matrix.drawPixel(30,6, matrix.Color333(170, 220, 200));
-	matrix.drawPixel(30,7, matrix.Color333(170, 220, 200));
-
-	matrix.drawPixel(0,0, matrix.Color333(255, 0, 0));
-	matrix.drawPixel(0,1, matrix.Color333(0, 255, 0));
-	matrix.drawPixel(0,2, matrix.Color333(0, 0, 255));
+  
+  // // T
+  // for (int i = 0; i < 31; i++) {
+  //   // Clear background
+  //   matrix.fillScreen(0);
+    
+  //   matrix.drawPixel(1 + i,0, matrix.Color333(200, 110, 70));
+  //   matrix.drawPixel(1 + i,1, matrix.Color333(200, 110, 70));
+  //   matrix.drawPixel(1 + i,2, matrix.Color333(200, 110, 70));
+  //   matrix.drawPixel(0 + i,1, matrix.Color333(200, 110, 70));
+  //   delay(100);
+  // }
+  Grid();
 }
 
 void run() { // Run the game
